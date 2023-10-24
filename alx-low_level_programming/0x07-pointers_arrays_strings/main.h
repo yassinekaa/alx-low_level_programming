5e7de0c466ddcd09a17acb7e1b6de01cc6c04b96
yassinekaa <136197518+yassinekaa@users.noreply.github.com>
2023-10-24 11:45:23 +0100

    main.h

--- alx-low_level_programming/0x07-pointers_arrays_strings/main.h
+++ alx-low_level_programming/0x07-pointers_arrays_strings/main.h
@@ -1,15 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_memset(char *s, char b, unsigned int n);
-int _putchar(char c);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char a[8][8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
-
-#endif
+char *_memset(char *s, char b, unsigned int n);  /* Fill memory with a constant byte */
+int _putchar(char c);  /* Write a character to stdout */
+char *_memcpy(char *dest, char *src, unsigned int n);  /* Copy memory area */
+char *_strchr(char *s, char c);  /* Locate character in string */
+unsigned int _strspn(char *s, char *accept);  /* Get the length of a prefix substring */
+char *_strpbrk(char *s, char *accept);  /* Search a string for any of a set of bytes */
+char *_strstr(char *haystack, char *needle);  /* Locate a substring in a string */
+void print_chessboard(char a[8][8]);  /* Print a chessboard */
+void print_diagsums(int *a, int size);  /* Print the sums of the diagonals in a matrix */
+void set_string(char **s, char *to);  /* Set the value of a pointer to a string */
